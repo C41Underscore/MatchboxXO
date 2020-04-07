e020bb72e721438a15dd62b9776c1f8e1874e7ca
Callum Methold <ed18cdmm@leeds.ac.uk>
2020-04-07 13:40:48 +0100

    Match AI weights worked on

--- AI.py
+++ AI.py
@@ -1,4 +1,41 @@
+from random import randint
+
 states = {}
+statesplayed = {}
+
+def formatstates():
+    for state in statesplayed.keys():
+        formattedWeights = 0
+        weightTotal = 0
+        weights = states[state].split(";")
+        weights = ",".join(weights)
+        weights[0][0] = weights[0][0].strip(":")
+        print(weights)
+
+
+def aisave():
+    with open("states.txt", "w") as statefile:
+        for state, weights in zip(states.keys(), states.values()):
+            print(state + ":" + weights, end="\n", file=statefile)
+
+def aiadjust(won):
+    for state in statesplayed.keys():
+        weights = states[state].split(";")
+        weights = [weights[0].split(","), weights[1].split(","), weights[2].split(",")]
+        weights[0][0] = weights[0][0].strip(":")
+        if won == 0:
+            weights[statesplayed[state][1]][statesplayed[state][0]] = str(int(weights[statesplayed[state][1]][statesplayed[state][0]]) - 2)
+            if int(weights[statesplayed[state][1]][statesplayed[state][0]]) < 0:
+                weights[statesplayed[state][1]][statesplayed[state][0]] = "0"
+        if won == 1:
+            weights[statesplayed[state][1]][statesplayed[state][0]] = str(int(weights[statesplayed[state][1]][statesplayed[state][0]]) + 2)
+        if won == 2:
+            weights[statesplayed[state][1]][statesplayed[state][0]] = str(int(weights[statesplayed[state][1]][statesplayed[state][0]]) + 1)
+        weights = [",".join(weights[0]), ",".join(weights[1]), ",".join(weights[2])]
+        weights = ";".join(weights)
+        states[state] = weights
+        formatstates()
+        aisave()
 
 def aiload():
     with open("states.txt", "r") as statefile:
@@ -12,10 +49,20 @@ def statecheck(state):
         if i == state:
             alreadySeen = True
     if not alreadySeen:
-        with open("states.txt", "a") as statefile:
-            print(state + ":2,2,2", end="\n", file=statefile)
+        states[state] = ":2,2,2;2,2,2;2,2,2"
+    return alreadySeen
 
 def aimove(board):
+    x = 0
+    y = 0
     boardState = ""
     boardState += "".join(board[0]) + "".join(board[1]) + "".join(board[2])
     statecheck(boardState)
+    x = randint(0, 2)
+    y = randint(0, 2)
+    while board[y][x] != "B":
+        x = randint(0, 2)
+        y = randint(0, 2)
+    board[y][x] = "X"
+    statesplayed[boardState] = (x, y)
+    return board
--- XO.py
+++ XO.py
@@ -1,6 +1,7 @@
-from AI import aiload, statecheck, aimove
+from AI import aiload, statecheck, aimove, aiadjust
+from random import randint
 
-board = [["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]
+board = list([["B", "B", "B"], ["B", "B", "B"], ["B", "B", "B"]])
 
 
 def printBoard():
@@ -8,30 +9,50 @@ def printBoard():
         for x in range(0, 3):
             print(board[x][i], end=" ")
         print(end="\n")
+    print(end="\n")
 
 def isWinner():
     for i in range(0, 3):
-        if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][1] != "0":
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][1] != "B":
             return 1
     for i in range(0, 3):
-        if board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[1][i] != "0":
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[1][i] != "B":
             return 1
-    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[1][1] != "0":
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[1][1] != "B":
         return 1
-    if board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[1][1] != "0":
+    if board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[1][1] != "B":
         return 1
+    return 0
+
 
 if __name__ == "__main__":
     aiload()
+    turnCount = 1
     playerXChoice = 0
     playerYChoice = 0
     print("Welcome to XO!")
     printBoard()
-    while isWinner() != 1:
-        print("X enter X choice: ", end="")
-        playerXChoice = int(input())
-        print("X enter Y choice: ", end="")
-        playerYChoice = int(input())
-        board[playerYChoice][playerXChoice] = "X"
+    while turnCount <= 9:
+        board = list(aimove(board))
+        if isWinner() == 1:
+            aiadjust(1)
+            break
         printBoard()
-        aimove()
+        turnCount += 1
+        if turnCount > 9:
+            break
+        playerXChoice = randint(0, 2)
+        playerYChoice = randint(0, 2)
+        while board[playerYChoice][playerXChoice] != "B":
+            playerXChoice = randint(0, 2)
+            playerYChoice = randint(0, 2)
+        board[playerYChoice][playerXChoice] = "O"
+        if isWinner() == 1:
+            aiadjust(0)
+            break
+        turnCount += 1
+        printBoard()
+    if turnCount > 9:
+        aiadjust(2)
+
+
